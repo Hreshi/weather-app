150f44a95ba745eb7d9e7d0ad764f81150c60519
hreshi <hkale746@gmail.com>
2022-02-03 09:49:28 +0530

    added method to get coordinates

--- src/main/java/org/aissms/WeatherServlet.java
+++ src/main/java/org/aissms/WeatherServlet.java
@@ -9,19 +9,48 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+
 public class WeatherServlet extends HttpServlet {
 
-	static String step1 = "http://api.openweathermap.org/geo/1.0/direct?q=%s&limit=%sappid=%s";
-	static String step2 = "api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s";
+	static String step1 = "https://api.openweathermap.org/geo/1.0/direct?q=%s&limit=%s&appid=%s";
+	static String step2 = "https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s";
+	static String apikey = "add api key here";
 
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
-		res.setContentType("application/json");
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
 
 		String city = req.getPathInfo();
 		city = city.substring(1, city.length());
-		out.println("{\"city\":\"" + city + "\"}");
+		String response = getWeatherData(city);
+		out.println(response);
 		out.flush();
+	}
+
+	private String getWeatherData(String city) throws IOException {
+		return getCoordinates(city);
+
+	}
+
+	private String getCoordinates (String city) throws IOException {
+		try {
+			String url = String.format(step1, city, 1, apikey);
+			System.out.println(url);
+			HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).GET().build();
+
+			HttpClient client = HttpClient.newHttpClient();
+			HttpResponse<String> res = client.send(request, BodyHandlers.ofString());
+			return res.body();
+		} catch (InterruptedException e) {
+			System.out.println(e);
+		}
+		return null;
 	}
 }
